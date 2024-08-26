9e69331570f7d4981e92423315b4b0851b515b11
juda <jud.zvi85@gmail.com>
2024-08-26 12:42:23 +0300

    over2newpc

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,6 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <p>This is the home page</p>
     </div>
-    // <form action={insertPost}>
-    //   <input type="date" name="date" required />
-    //   <input type="text" name="title" placeholder="title" required />
-    //   <input type="number" name="time" placeholder="time" required />
-    //   <button type="submit">Send</button>
-    // </form>
   )
 }
--- test/20240816052553_gen_name/migration.sql
+++
@@ -1,4 +0,0 @@
--- This is an empty migration.
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "name" TEXT GENERATED ALWAYS AS ("firstName" || ' ' || "lastName") STORED;
-ALTER TABLE "Creator" ADD COLUMN     "name" TEXT GENERATED ALWAYS AS ("firstName" || ' ' || "lastName") STORED;
